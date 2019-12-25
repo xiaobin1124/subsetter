8cd26a481b94c548de0f5a28dd2999e6a22989a8
Bin Xiao <xiaobin@fio.org.cn>
2019-12-25 09:10:59 +0800

    Manditory input options and exists of config_files.

--- subsetter.sh
+++ subsetter.sh
@@ -2,16 +2,22 @@
 # regrid output for specific Region, xiaob 20190129
 # Original z-level
 set -x
-ymdh=$1
-fname_dir=$2
+#ymdh=$1
+if [[ $# -ne 3 ]];then
+  echo Manditory input options: directory of filename, varname and config file.
+  echo example: ./subsetter.sh /path/to/file/19800101.ocean_temp.nc temp config_file
+  exit 1
+fi
+fname_dir=$1
 output_prefix=d17
 #res_dir=${F_HOME}dataout/$ymdh
 fname=`basename ${fname_dir}`
-vname=$3
+vname=$2
+config_file=$3
 echo $fname_dir
 #echo $res_dir
 echo $fname
-output_dir=/SUGONP300-1/data_collab/Solomon
+output_dir=/SUGONP300-1/data_collab1/zhangzx
 do_regrid_vname=(temp salt u v)
 do_regrid_z=0
 do_regrid_z=1
@@ -20,31 +26,37 @@ xregion="x=145.0:165.0"
 yregion="y=-15.:-4."
 zregion="z=1:1000"
 
-out_dir_tmp=${output_dir}/tmp/$ymdh
-dataout=${output_dir}/$ymdh
-jnl_dir=${output_dir}/jnl
-jnl=${output_dir}/$fname.jnl
-/bin/bash subsetter_config 
-
+if [ ! -f $config_file ];then
+  echo please confirm config_file: $config_file exits!
+  exit 1
+fi
 xcoor=XT_OCEAN      #default value of xcoor
 scale_factor=1.     #default value of scale_factor
 add_offset=0.       #default value of add_offset
 outtype=float       #default value of outtype
+source `dirname $config_file`/`basename $config_file`
+#out_dir_tmp=${output_dir}/tmp/$ymdh
+out_dir_tmp=${output_dir}/tmp/
+#dataout=${output_dir}/$ymdh
+dataout=${output_dir}/
+jnl_dir=${output_dir}/jnl
+jnl=${output_dir}/$fname.jnl
+
 if [[ $vname == *u* ]] || [[ $vname == *v* ]];then
 #  fname=tco.$fname 
-  xcoor=XU_OCEAN
+  #xcoor=XU_OCEAN
   scale_factor=0.0005
   add_offset=0.
   outtype=short
 fi
 if [[ $vname == *salt* ]];then
-  xcoor=XT_OCEAN
+  #xcoor=XT_OCEAN
   scale_factor=0.001
   add_offset=25.
   outtype=short
 fi
 if [[ $vname == *temp* ]];then
-  xcoor=XT_OCEAN
+  #xcoor=XT_OCEAN
   scale_factor=0.00125
   add_offset=30.
   outtype=short
