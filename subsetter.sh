164ddd0f339c4793df5ac63785d3f0a0da2f61b3
Bin Xiao <xiaobin@fio.org.cn>
2020-10-16 12:11:51 +0800

    An important update. can mode upercase ncrename -h sed commands

--- subsetter.sh
+++ subsetter.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # regrid output for specific Region, xiaob 20190129
 # Original z-level
-#set -x
+set -x
 set -e
 #ymdh=$1
 if [[ $# -ne 3 ]];then
@@ -20,15 +20,13 @@ echo $fname_dir
 echo $fname
 output_dir=/SUGONP300-1/data_collab1/zhangzx
 do_regrid_vname=(temp salt u v)
-do_regrid_z=0
-do_regrid_z=1
 var_out=$vname
-xregion="x=145.0:165.0"
-yregion="y=-15.:-4."
-zregion="z=1:1000"
+#xregion="x=145.0:165.0"
+#yregion="y=-15.:-4."
+#zregion="z=1:1000"
 
 if [ ! -f $config_file ];then
-  echo please confirm config_file: $config_file exits!
+  echo "please confirm config_file: $config_file exits!"
   exit 1
 fi
 xcoor=XT_OCEAN      #default value of xcoor
@@ -45,30 +43,27 @@ fi
 dataout=${output_dir}/
 jnl=${output_dir}/$fname.jnl
 
-if [[ $vname == *u* ]] || [[ $vname == *v* ]];then
+if [[ ${vname,,} == *u* ]] || [[ ${vname,,} == *v* ]];then
 #  fname=tco.$fname 
   #xcoor=XU_OCEAN
   scale_factor=0.0005
   add_offset=0.
   outtype=short
 fi
-if [[ $vname == *salt* ]];then
+if [[ ${vname,,} == *salt* ]];then
   #xcoor=XT_OCEAN
   scale_factor=0.001
   add_offset=25.
   outtype=short
 fi
-if [[ $vname == *temp* ]];then
-  xcoor=XT_OCEAN
+if [[ ${vname,,} == *temp* ]];then
+  #xcoor=XT_OCEAN
   scale_factor=0.00125
   add_offset=30.
   outtype=short
 fi
 fname_dir=`dirname $fname_dir`/$fname
 echo $fname_dir
-if [ $do_regrid_z != 1 ];then
-  exit 0
-fi
 if [ ! -d $out_dir_tmp ];then
   mkdir -p $out_dir_tmp
 fi
@@ -82,6 +77,7 @@ echo Run.regrid_mmd: $fname
 if [ -z $zlev ];then #IF
 
 cat > $jnl << EOF
+CANCEL MODE upcase_output
 use "$fname_dir"
 set mem/size=2000
 set axis/modulo $xcoor
@@ -89,7 +85,7 @@ set axis/modulo $xcoor
 !define axis/$yregion/units=degree_north yax
 !define axis/z zax={$zlev}
 !define grid/x=xax/y=yax/z=zax gg
-set region/$xregion/$yregion/$zregion
+set region/$xregion/$yregion/$zregion/$tregion
 
 let vmax=32767*$scale_factor+$add_offset
 let vmin=-32767*$scale_factor+$add_offset
@@ -104,6 +100,7 @@ EOF
 else                  #else 
 
 cat > $jnl << EOF
+CANCEL MODE upcase_output
 use "$fname_dir"
 set mem/size=2000
 set axis/modulo $xcoor
@@ -124,12 +121,16 @@ save/file="$out_dir_tmp/${output_prefix}.${fname}"/clobber ${var_out}1[gz=ggz]
 EOF
 
 fi                    #FI
+sed -i 's/\/\/\/\/$//g' $jnl;sed -i 's/\/\/\/$//g' $jnl;sed -i 's/\/\/$//g' $jnl;sed -i 's/\/$//g' $jnl;sed -i 's/\/\//\//g' $jnl;sed -i 's/\/\//\//g' $jnl;sed -i 's/\/\/\//\//g' $jnl
 ferret -script $jnl
 if [ $? -ne 0 ];then exit 1;fi
 #nccopy -d6 $out_dir/d12.${fname} $dataout/d12.${fname}
 mv $out_dir_tmp/${output_prefix}.${fname} $dataout/${output_prefix}.${fname}
-gzip $dataout/${output_prefix}.${fname}
+#gzip $dataout/${output_prefix}.${fname}
+nccopy -d6 $dataout/${output_prefix}.${fname} $dataout/${output_prefix}.${fname}.d6
+mv $dataout/${output_prefix}.${fname}.d6 $dataout/${output_prefix}.${fname}
+ncrename -h -v ${var_out}1,${var_out} -O $dataout/${output_prefix}.${fname}
 #rm $out_dir/d12.${fname}
 rm $jnl
-rm -r out_dir_tmp
+#rm -r $out_dir_tmp
 exit 0
